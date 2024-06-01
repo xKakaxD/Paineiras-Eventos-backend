cfb22cabc98bd20cab0e00d5e4cf6b7c2fb914ad
xKakaxD <ti.demoro@gmail.com>
2024-05-31 22:31:44 -0300

    feat: mudança do tema para sistema de gestão de eventos de condomínio, alterando o necessário para o novo escopo

--- src/trabalho/backend/BancoDeDados.java
+++ src/trabalho/backend/BancoDeDados.java
@@ -17,14 +17,13 @@ public class BancoDeDados implements PersistenceService{
 	public static void main(String[] args) {
 		inicializa();
 	}
- 
 	
 	public static void inicializa() {
-		Usuario usuario1 = new Usuario(1, "Kaká","14602053670","321","xkakaxd@gmail.com","Rua Ja To Inno,80-Barreiro","8/10/1999",'1');
-		Usuario usuario2 = new Usuario(2, "Luis","11111111111","123","luis@email.com","Rua dos Pássaros,123-Bairro Alto da Serra","14/12/1979",'1');
-		Usuario usuario3 = new Usuario(3, "Giovanni","22222222222","123","giovanni@email.com","Avenida dos Astronautas, 456-Condomínio Estrela Cadente, Bloco C, Apto 302","28/07/1989",'2');
-		Usuario usuario4 = new Usuario(4, "Wanderleco","33333333333","123", "wandeco@email.com,","Avenida dos Girassóis, 456-Bairro Vista Bela","4/07/2000",'0');
-		Usuario usuario5 = new Usuario(5, "Rodrigo","44444444444","123","roddoido@gmail.com","Rua das Flores, 123, Cidade Feliz, Estado da Alegria","15/07/1990",'1');
+		Usuario usuario1 = new Usuario(1, "Kaká","14602053670","321","xkakaxd@gmail.com","31995290543","8/10/1999",'1','1');
+		Usuario usuario2 = new Usuario(2, "Luis","11111111111","123","luis@email.com","31995290543","14/12/1979",'1','2');
+		Usuario usuario3 = new Usuario(3, "Giovanni","22222222222","123","giovanni@email.com","31995290543","28/07/1989",'2','3');
+		Usuario usuario4 = new Usuario(4, "Wanderleco","33333333333","123", "wandeco@email.com,","31995290543","4/07/2000",'0','3');
+		Usuario usuario5 = new Usuario(5, "Sândalo","44444444444","123","bessa@gmail.com","31995290543","15/07/1990",'1','0');
 		
 		mapa.put(usuario1.getCpf(), usuario1);
 		mapa.put(usuario2.getCpf(), usuario2);
@@ -43,7 +42,6 @@ public class BancoDeDados implements PersistenceService{
         }
         return lista;
     }
-    
     
 	@Override
 	public <T> T recupera(String key) {
